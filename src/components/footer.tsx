8b2bf05682c4c2a848d2b7ac59d455ffaf53e0d8
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-11 22:50:13 +0300

    added footer file

---
+++ src/components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-sm text-black">
+          &Copy:2024 KDF Store ,Inc,All right reserved
+        </p>
+      </div>
+    </footer>
+  );
+}
