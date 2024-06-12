f6eed4bf5b5d88260c8ea8b1b52b735f2bc57a3f
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-13 02:08:46 +0300

    WIP:added   fillter file

---
+++ src/app/category/[categoryId]/_components/fillter.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { useRouter, useSearchParams } from "next/navigation";
+import React from "react";
+import { Color, Size } from "../../../../../type";
+import qs from "query-string";
+import { Button } from "@/components/ui/button";
+
+interface FilterProps {
+  valueKey: string;
+  name: string;
+  data: (Size | Color)[];
+}
+export default function Filter({ valueKey, name, data }: FilterProps) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const selectedValue = searchParams.get(valueKey);
+
+  const onClick = (id: string) => {
+    const current = qs.parse(searchParams.toString());
+    const query = {
+      ...current,
+      [valueKey]: id,
+    };
+    if (current[valueKey] === id) {
+      query[valueKey] = null;
+    }
+    const url = qs.stringifyUrl(
+      { url: window.location.href, query },
+      { skipNull: true }
+    );
+    router.push(url);
+  };
+  return (
+    <div className="mb-8">
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <hr className="my-4" />
+      <div className="flex flex-wrap gap-2">
+        {data.map((filter) => (
+          <div className="flex items-center" key={filter.id}>
+            <Button>{filter.name}</Button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
