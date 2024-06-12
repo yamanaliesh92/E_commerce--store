00e399a8f207f3b10fc5ad4f75c15a06dc71da36
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 03:04:45 +0300

    WIP:added default style to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,3 +1,4 @@
+import { cn } from "@/lib/utils";
 import { forwardRef } from "react";
 
 export interface ButtonProps
@@ -5,7 +6,17 @@ export interface ButtonProps
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
-    return <button ref={ref}>{children}</button>;
+    return (
+      <button
+        className={cn(
+          `w-auto rounded-full bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed text-white disabled:opacity-50 font-semibold hover:opacity-75 transition`,
+          className
+        )}
+        ref={ref}
+      >
+        {children}
+      </button>
+    );
   }
 );
 
