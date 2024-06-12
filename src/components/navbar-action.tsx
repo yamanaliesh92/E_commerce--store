cc08006f09e5e0d565f37425c96fdda34dd0dda0
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 03:07:23 +0300

    added button-file to  navbar-action file

--- src/components/navbar-action.tsx
+++ src/components/navbar-action.tsx
@@ -1,14 +1,22 @@
 "use client";
 
 import { ShoppingBag } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "./ui/button";
 
 export default function NavbarAction() {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) {
+    return null;
+  }
   return (
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center bg-black px-4 rounded-full py-2">
         <ShoppingBag size={20} color="white" />
+        <span className="ml-2 text-sm text-white font-medium">0</span>
       </Button>
     </div>
   );
