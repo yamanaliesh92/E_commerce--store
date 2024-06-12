f13250dd7a32eeeda79c10bb8ee687e208180611
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-13 01:08:12 +0300

    WIP:added some updates fot  interface in  category page

--- src/app/category/[categoryId]/page.tsx
+++ src/app/category/[categoryId]/page.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { getProducts } from "../../../../actions/get-products";
 
 interface CategoryPageProps {
-  categoryId: string;
-  searchParams: string;
+  params: { categoryId: string };
+  searchParams: {
+    colorId: string;
+    sizeId: string;
+  };
 }
 
-export default function CategoryPage() {
+export default async function CategoryPage({
+  searchParams,
+  params,
+}: CategoryPageProps) {
+  const products = await getProducts({ categoryId: params.categoryId });
   return <div></div>;
 }
