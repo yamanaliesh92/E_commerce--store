ab973409fed654e1a610e834340f8c2a6fbf3c29
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 12:34:03 +0300

    WIP:added no-result file to product-list file

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Product } from "../../type";
+import NoResult from "./ui/no-result";
 
 interface ProductListProps {
   title: string;
@@ -10,6 +11,7 @@ export default function ProductList({ title, items }: ProductListProps) {
   return (
     <div className="space-y-4">
       <h3 className="font-bold text-3xl">{title}</h3>
+      {items.length === 0 && <NoResult />}
     </div>
   );
 }
