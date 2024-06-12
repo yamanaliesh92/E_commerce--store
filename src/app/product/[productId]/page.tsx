59ca72b44f2d562a64696be32407b2fe1387fec3
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 18:04:42 +0300

    WIP:added style to product-page

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,3 +1,4 @@
+import Container from "@/components/ui/container";
 import React from "react";
 
 export default function ProductPage({
@@ -5,5 +6,16 @@ export default function ProductPage({
 }: {
   params: { productId: string };
 }) {
-  return <div></div>;
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-10 sm:px-6 lg:px-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+            <div>Gallary</div>
+            <div className="mt-10 px-4 sm:mt-16 lg:mt-0">Info</div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
