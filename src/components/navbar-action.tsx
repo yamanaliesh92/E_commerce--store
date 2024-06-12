fb3f47f817de77123f799ba49adb8d662f3f8799
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 03:01:51 +0300

    WIP:added cn to utils file

--- src/components/navbar-action.tsx
+++ src/components/navbar-action.tsx
@@ -1,7 +1,15 @@
 "use client";
 
+import { ShoppingBag } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 export default function NavbarAction() {
-  return <div className="ml-auto flex items-center gap-x-4"></div>;
+  return (
+    <div className="ml-auto flex items-center gap-x-4">
+      <Button className="flex items-center bg-black px-4 rounded-full py-2">
+        <ShoppingBag size={20} color="white" />
+      </Button>
+    </div>
+  );
 }
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { forwardRef } from "react";
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
-const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, children, disabled, type = "button", ...props }, ref) => {
     return <button ref={ref}>{children}</button>;
   }
