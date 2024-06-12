2a83bc1c511196ecfaba1fadb9a0b6953779e486
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-12 03:38:06 +0300

    added interface for product

--- type.ts
+++ type.ts
@@ -9,3 +9,14 @@ export interface Category {
   name: string;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  name: string;
+  price: string;
+  category: Category;
+  isFeatured: boolean;
+  size: Size[];
+  color: Color[];
+  images: Image[];
+}
