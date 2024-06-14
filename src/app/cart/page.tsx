e7117e365f7f49d56ffe57ab93d3a82784398f94
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-14 14:09:42 +0300

    WIP:added  cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,23 @@
+import Container from "@/components/ui/container";
+import React from "react";
+import { useCart } from "../../../hooks/use-cart";
+
+export default function CartPage() {
+  const cart = useCart();
+  return (
+    <div className="bg-white">
+      <Container>
+        <div className="px-4 py-16 sm:px-6 lg:px-8 ">
+          <h1 className="text-3xl font-bold text-black">Shopping Cart</h1>
+          <div className="mt-12 lg:grid lg:grid-cols-12 lg:items-start gap-x-12">
+            <div className="lg:col-span-7">
+              {cart.items.length === 0 && (
+                <p className="text-neutral-500">No items added it cart</p>
+              )}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+}
