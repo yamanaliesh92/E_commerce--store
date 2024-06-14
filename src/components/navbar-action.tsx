a191821d9a4b5cba04a09aaedf65f84be6817d50
Yaman Aliesh <yaman9892xxx@gmail.com>
2024-06-14 12:38:37 +0300

    added use-cart to navbar-action

--- src/components/navbar-action.tsx
+++ src/components/navbar-action.tsx
@@ -2,6 +2,7 @@
 
 import { ShoppingBag } from "lucide-react";
 import React, { useEffect, useState } from "react";
+import { useCart } from "../../hooks/use-cart";
 import { Button } from "./ui/button";
 
 export default function NavbarAction() {
@@ -9,6 +10,8 @@ export default function NavbarAction() {
   useEffect(() => {
     setIsMounted(true);
   }, []);
+
+  const cart = useCart();
   if (!isMounted) {
     return null;
   }
@@ -16,7 +19,9 @@ export default function NavbarAction() {
     <div className="ml-auto flex items-center gap-x-4">
       <Button className="flex items-center bg-black px-4 rounded-full py-2">
         <ShoppingBag size={20} color="white" />
-        <span className="ml-2 text-sm text-white font-medium">0</span>
+        <span className="ml-2 text-sm text-white font-medium">
+          {cart.items.length}
+        </span>
       </Button>
     </div>
   );
